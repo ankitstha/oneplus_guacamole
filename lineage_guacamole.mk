2be10461feda35b95d074500b0f37de55706b217
Ankit Shrestha <ankitshrestha114@gmail.com>
2024-02-13 11:28:08 +0545

    Bringup: Matrixx

---
+++ lineage_guacamole.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
+$(call inherit-product, device/oneplus/guacamole/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Matrixx Flags
+MATRIXX_MAINTAINER := c00chiemilker
+MATRIXX_CHIPSET := Snapdragon855
+MATRIXX_BATTERY := 4000mAh
+MATRIXX_DISPLAY := 1440x3120
+WITH_GMS := true
+TARGET_HAS_UDFPS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+TARGET_DISABLE_EPPE := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_guacamole
+PRODUCT_DEVICE := guacamole
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := GM1917
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_SYSTEM_NAME := OnePlus7Pro
+PRODUCT_SYSTEM_DEVICE := OnePlus7Pro
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Boot animation
+scr_resolution := 1440
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+
+# Build info
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus7Pro \
+    TARGET_NAME=OnePlus7Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
---
+++ setup-makefiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=guacamole
+export DEVICE_COMMON=sm8150-common
+export VENDOR=oneplus
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
