0605d300ca78be3024b2752a9af32881fa36a1b5
Ankit Shrestha <ankitshrestha114@gmail.com>
2024-02-26 08:12:51 +0545

    Bringup: SuperiorExtended

--- superior_guacamole.mk
+++ superior_guacamole.mk
@@ -11,11 +11,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common SuperiorExtendedOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+# Superior Flags
+TARGET_SHIPS_PREBUILT_GCAM := false
+TARGET_INCLUDE_MATLOG := false
+USE_MOTO_CALCULATOR := false
+TARGET_SUPPORTS_BLUR := true
+SUPERIOR_UDFPS_ANIMATIONS := true
+USE_MOTO_CLOCK := false
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+BUILD_WITH_GAPPS := true
+USE_DUCKDUCKGO := false
+USE_ViaBrowser := false
+TARGET_BUILD_VIMUSIC := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_guacamole
+PRODUCT_NAME := superior_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := GM1917
